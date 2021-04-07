c73db8bb9576bd44700867c3bf8aac22500ed379
AdeelAhsan <adeelahsan5640@gmail.com>
2021-04-07 22:23:29 +0500

    made changes

---
+++ npmMode/index.js
@@ -0,0 +1,4 @@
+const chalk =require('chalk');
+const validator=require('validator');
+console.log(chalk.blue.underline.inverse('Helloworld'));
+console.log(validator.isEmail('adeelahsan5640@gmail.com'));
